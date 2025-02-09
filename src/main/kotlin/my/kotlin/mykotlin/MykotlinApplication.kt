1542bae7bce91a4905f1dd4f9b97db43a29ab2c8
Pavel.Alekseev <pavel.a@kyla.com>
2025-02-09 01:50:53 -0600

    initial

---
+++ src/main/kotlin/my/kotlin/mykotlin/MykotlinApplication.kt
@@ -0,0 +1,11 @@
+package my.kotlin.mykotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MykotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<MykotlinApplication>(*args)
+}
---
+++ src/test/kotlin/my/kotlin/mykotlin/MykotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.kotlin.mykotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MykotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
